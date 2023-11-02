d53d8efe2630c9b4af745de160a0f26a88cee0c2
jay gandhi <jaygandhi3004@gmail.com>
2023-11-02 13:25:18 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Localstorage from './Crud/Localstorage';
+
+function App() {
+  return (
+    <div className="App">
+      <Localstorage />
+    </div>
+  );
+}
+
+export default App;
